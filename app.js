cbc8189d7852aba485bc32bde98f82287bfd1e3c
nose <nose@gmail.com>
2024-04-17 18:07:43 -0500

    dinamico

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./database/connection');
+const Coffee = require('./models/Coffee'); // Asegúrate de ajustar la ruta según tu estructura de archivos
 
 // Configuración para servir archivos estáticos desde el directorio 'public'
 app.use(express.static('public'));
@@ -15,17 +16,34 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', { nombre: 'Usuario Pug' });
 });
+
 app.get('/contact', (req, res) => {
     res.render('contact', { nombre: 'Usuario Pug' });
 });
-app.get('/menu', (req, res) => {
-    res.render('menu', { nombre: 'Usuario Pug' });
+
+app.get('/menu', async (req, res) => {
+    try {
+        const coffees = await Coffee.find({});
+        console.log(coffees)
+        res.render('menu', { coffees });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Hubo un error al obtener los cafés');
+    }
 });
 
 app.engine('ejs', require('ejs').renderFile);
 
-app.get('/ejs', (req, res) => {
-    res.render('index.ejs', { nombre: 'Usuario EJS' });
+app.get('/reservation', (req, res) => {
+    res.render('reservation.ejs', { nombre: 'Usuario EJS' });
+});
+
+app.get('/service', (req, res) => {
+    res.render('service.ejs', { nombre: 'Usuario EJS' });
+});
+
+app.get('/testimonial', (req, res) => {
+    res.render('testimonial.ejs', { nombre: 'Usuario EJS' });
 });
 
 app.listen(3000, () => {
